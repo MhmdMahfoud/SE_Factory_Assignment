7209b38fe80a4c56386a16d26e5e45df650f2c44
MhmdMahfoud <engmohammedmahfoud2001@gmail.com>
2025-06-15 21:15:15 +0300

    This is the solution of the assigment

--- First_Assignment.py
+++ First_Assignment.py
@@ -6,3 +6,27 @@ while Choices !=4:
 2. Deposit Money
 3. Withdraw Money
 4. Exit""")
+    Choices =int(input("your choice is "))
+    if Choices==1:
+        print("Your current balance is " + str(Balance) +"$")
+    elif Choices == 2:
+        amount=float(input("how much to deposit ?"))
+        if amount >0:
+            Balance=Balance+amount
+            print("Your New Balance  is" +str(Balance))
+        else:
+            print("amount should be greater then 0")
+    elif Choices == 3:
+        amount=float(input("how much to witdraw  "))
+        if Balance > amount:
+            Balance=Balance-amount
+            print("the new balance is  " +str(Balance))
+        else:
+            print("no enough balance ")
+    elif Choices == 4:
+        print("exit")
+    else:
+        print("Invalid")
+else:
+    print("password incorrect")
+    
