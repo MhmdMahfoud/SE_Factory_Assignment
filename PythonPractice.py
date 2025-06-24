e6a40d58c558484d45d2da18091400d57b475836
Mhmd Mahfoud <engmohammedmahfoud2001@gmail.com>
2025-06-24 11:00:15 +0300

    Add The Records

---
+++ PythonPractice.py
@@ -0,0 +1,6 @@
+records = [
+["Layla", 89], ["Tariq", 77], ["Layla", 91], ["Jana", 100], ["Tariq", 84],
+["Ziad", 62], ["Jana", 97], ["Tariq", 73], ["Ziad", 71], ["Layla", 86],
+["Jana", 94], ["Ziad", 75]
+]
+print(records)
